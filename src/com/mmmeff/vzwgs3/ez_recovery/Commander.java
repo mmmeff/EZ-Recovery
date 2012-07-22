eb4320748fdd8f9c6b65f1474b23e7e02387b741
Matthew Frey <matthewfrey118@gmail.com>
2012-07-22 05:07:49 -0400

    Completed recovery-flashing panel
    Needs extended testing, but I'm leaving for vacation in 2 hours! Gotta
    get some sleep ;)

--- src/com/mmmeff/vzwgs3/ez_recovery/Commander.java
+++ src/com/mmmeff/vzwgs3/ez_recovery/Commander.java
@@ -4,7 +4,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /** serves as an interface for running console commands in the android os **/
 public class Commander {
@@ -32,7 +34,7 @@ public class Commander {
 	 * Run a single command while disregarding any output given
 	 * @param command
 	 */
-	public void ExecSingle(String command){
+	private void ExecSingle(String command){
 		try {
 			input.writeBytes(command + "\n");
 			input.flush();
@@ -47,7 +49,7 @@ public class Commander {
 	 * @param command
 	 * @return command output
 	 */
-	public String ExecSingleWithResults(String command){
+	private String ExecSingleWithResults(String command){
 		try {
 			input.writeBytes(command + "\n");
 			input.flush();
@@ -59,19 +61,21 @@ public class Commander {
 		}
 	}
 	
-	public void ExecMulti(String[] commands){
+	private boolean ExecMulti(String[] commands){
 		try {
 			for (String command : commands){
 				input.writeBytes(command + "\n");
 				input.flush();
 			}
+			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
 			Log.e(TAG, e.getMessage());
+			return false;
 		}
 	}
 	
-	public String[] ExecMultiWithResults(String[] commands){
+	private String[] ExecMultiWithResults(String[] commands){
 		String[] res = new String[commands.length];
 		try {
 			for(int i = 0; i < commands.length; i++){
@@ -85,5 +89,35 @@ public class Commander {
 			Log.e(TAG, e.getMessage());
 			return null;
 		}
+	}
+
+	public boolean FlashRecovery(String title) {
+		String path = "/sdcard/gs3ezrecovery/";
+		//due to java not supporting string switches until 1.7
+		//and android requiring java 1.5 or 1.6, please forgive
+		//this long and ugly if else chain :/
+		if (title.equals("CWM Touch 5.8.4.9")){
+			path += "recovery_rec_CWMT5849";
+		} else if (title.equals("CWM 6.0.1.0")){
+			path += "recovery_rec_CWM6010";
+		} else if (title.equals("TWRP 2.2.0")){
+			path += "recovery_rec_twrp220";
+		} else if (title.equals("Invisblek v2 Kernel")){
+			path += "recovery_hyb_invisiblekv2.img";
+		} 
+		
+		path += ".img";
+		
+		String[] commands = { "dd if=" + path + " of=/dev/block/mmcblk0p18\n", "exit\n" };
+		return ExecMulti(commands);
+	}
+
+	public boolean FlashCustomRecovery(String pathToRecovery) {
+		String[] commands = { "dd if=" + pathToRecovery + " of=/dev/block/mmcblk0p18\n", "exit\n" };
+		return ExecMulti(commands);
+	}
+
+	public void rebootRecovery() {
+		ExecSingle("reboot recovery");
 	}
 }
--- src/com/mmmeff/vzwgs3/ez_recovery/MyPagerAdapter.java
+++ src/com/mmmeff/vzwgs3/ez_recovery/MyPagerAdapter.java
@@ -1,29 +1,42 @@
 package com.mmmeff.vzwgs3.ez_recovery;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MyPagerAdapter extends PagerAdapter {
 
-	Context context;
+	protected static final String TAG = "ezrecovery-MyPagerAdapter";
+
+	private Commander commander;
 	
-	Spinner recovery_recoverySpinner;
-	Spinner recovery_hybridSpinner;
-	ArrayAdapter<CharSequence> recovery_recoveryAdapter;
-	ArrayAdapter<CharSequence> recovery_hybridAdapter;
+	private Context context;
+	private Spinner recovery_recoverySpinner, recovery_hybridSpinner;
+	private ArrayAdapter<CharSequence> recovery_recoveryAdapter, recovery_hybridAdapter;
+	private Button browseButton, flashButton, rebootButton;
+	private EditText customRecoveryPathField;
+	private RadioGroup recoveryRadioGroup;
 	
 	public MyPagerAdapter(Context context){
 		this.context = context;
+		this.commander = new Commander();
 	}
 	
 	public int getCount() {
-        return 3;
+        return 4;
     }
 
     public Object instantiateItem(View collection, int position) {
@@ -42,6 +55,18 @@ public class MyPagerAdapter extends PagerAdapter {
             resId = R.layout.recovery_layout;
             view = inflater.inflate(resId, null);
             
+            //load components
+            customRecoveryPathField = (EditText) view.findViewById(R.id.pathField);
+            recoveryRadioGroup = (RadioGroup) view.findViewById(R.id.radioGroup);
+            
+            //load radio buttons
+            
+            
+            //load buttons
+            browseButton = (Button) view.findViewById(R.id.browseButton);
+            flashButton = (Button) view.findViewById(R.id.flashButton);
+            rebootButton = (Button) view.findViewById(R.id.rebootButton);
+            
             //set up recovery choices in spinner
             recovery_recoverySpinner = (Spinner) view.findViewById(R.id.recovery_recovery_spinner);
             recovery_recoveryAdapter = ArrayAdapter.createFromResource(
@@ -58,9 +83,71 @@ public class MyPagerAdapter extends PagerAdapter {
     		recovery_hybridAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     		recovery_hybridSpinner.setAdapter(recovery_hybridAdapter);
             
+    		//configure button listeners
+    		OnClickListener flashListener = new OnClickListener() {
+                public void onClick(View v) {
+                	boolean success;
+                	int selection = recoveryRadioGroup.getCheckedRadioButtonId();
+                	switch (selection){
+                		case R.id.recovery_radioButtonCustom:
+                			success = commander.FlashCustomRecovery(customRecoveryPathField.getText().toString());
+                			break;
+                		case R.id.recovery_radioButtonRecovery:
+                			success = commander.FlashRecovery((String) recovery_recoverySpinner.getSelectedItem());
+                			break;
+                		case R.id.recovery_radioButtonHybrid:
+                			success = commander.FlashRecovery((String) recovery_hybridSpinner.getSelectedItem());
+                			break;
+                		case R.id.recovery_radioButtonStock:
+                			success = commander.FlashRecovery("recovery_rec_stock.img");
+                			break;
+                		default:
+                			Toast toast = Toast.makeText(context, "Please make a selection.", Toast.LENGTH_SHORT);
+                	    	toast.show();
+                	    	success = false;
+                	    	break;
+                	}
+                	if (!success){
+                		Toast toast = Toast.makeText(context, "Recovery Flash Failed.", Toast.LENGTH_SHORT);
+            	    	toast.show();
+                	} else {
+                		Toast toast = Toast.makeText(context, "Recovery Flash Successful!", Toast.LENGTH_SHORT);
+            	    	toast.show();
+                	}
+                }	
+            };
+    		flashButton.setOnClickListener(flashListener);
+    		
+    		OnClickListener rebootListener = new OnClickListener() {
+    			public void onClick(View v) {
+    				DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+    				    public void onClick(DialogInterface dialog, int which) {
+    				        switch (which){
+    				        case DialogInterface.BUTTON_POSITIVE:
+    				        	Log.d("tag", "Rebooting into recovery.");
+    				            commander.rebootRecovery();
+    				            break;
+
+    				        case DialogInterface.BUTTON_NEGATIVE:
+    				            //No button clicked
+    				            break;
+    				        }
+    				    }
+    				};
+    				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    				builder.setMessage("Are you sure you want to reboot into recovery?").setPositiveButton("Yes", dialogClickListener)
+    				    .setNegativeButton("No", dialogClickListener).show();
+    			}
+    		};
+    		rebootButton.setOnClickListener(rebootListener);
+    		
             break;
         case 2:
             resId = R.layout.kexec_layout;
+            view = inflater.inflate(resId, null);
+            break;
+        case 3:
+            resId = R.layout.romcentral_layout;
             view = inflater.inflate(resId, null);
             break;
         
@@ -97,6 +184,7 @@ public class MyPagerAdapter extends PagerAdapter {
         	case 0: return context.getResources().getString(R.string.title_section3);
         	case 1: return context.getResources().getString(R.string.title_section1);    
         	case 2: return context.getResources().getString(R.string.title_section2);
+        	case 3: return context.getResources().getString(R.string.title_section4);
         }
         return null;
     }
